99bcb6ece37d5862bfd1824de5f3f446806fbd05
yhanyi <yeohhanyi0916@gmail.com>
2024-08-03 20:31:09 +0800

    Updated data structs

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,5 @@
+use chrono::{ DateTime, Utc };
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum OrderType {
     Buy,
@@ -7,15 +9,33 @@ pub enum OrderType {
 #[derive(Debug, Clone)]
 pub struct Order {
     pub id: u64,
+    pub trading_pair: TradingPair,
     pub order_type: OrderType,
     pub price: f64,
-    pub quantity: u32,
+    pub quantity: f64,
+    pub timestamp: DateTime<Utc>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct TradingPair {
+    pub base: String,
+    pub quote: String,
 }
 
 #[derive(Debug)]
 pub struct Trade {
+    pub id: u64,
+    pub trading_pair: TradingPair,
     pub buy_order_id: u64,
     pub sell_order_id: u64,
     pub price: f64,
-    pub quantity: u32,
+    pub quantity: f64,
+    pub timestamp: DateTime<Utc>,
+}
+
+#[derive(Debug)]
+pub struct PriceUpdate {
+    pub trading_pair: TradingPair,
+    pub price: f64,
+    pub timestamp: DateTime<Utc>,
 }
